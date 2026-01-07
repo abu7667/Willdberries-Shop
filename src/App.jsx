3946622d39c4191db64252675c3119cab736bf12
SaidSwattt <yan.lyubimov.08@mail.ru>
2026-01-07 17:02:10 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,7 @@ function App() {
   return (
     <>
      <Home />
+     <Footer />
     </>
   )
 }
